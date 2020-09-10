84d3c9953c227ce7b23924e2e7ca3280dd59fdbf
quockhanh22 <anhkhanh11761@gmail.com>
2020-09-10 10:46:06 +0700

    chuyen doi tien te
    chuyen doi tien te

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace chuyendoitiente
+{
+    class Program
+    {
+    public static void doitien(){
+            int vnd,usd=1;
+            vnd = usd * 23000;
+           Console.WriteLine("{0} USD = {1} VND ", usd, vnd);
+            Console.Read();
+
+        }
+        static void Main(string[] args)
+        {
+             int usd;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            Console.WriteLine("Nhập số tiền USD muốn chuyển:");
+            usd = Convert.ToInt32(Console.ReadLine());
+           doitien();
+        
+        }
+    }
+}
